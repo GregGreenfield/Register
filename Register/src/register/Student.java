228af5beb4544a45d595ace5817dd81f5b9db1ce
Greg Greenfield <g.r.greenfield@gmail.com>
2016-05-14 17:47:06 +1200

    Initial commit
    Fixing up the extra stuff like Binary files from GIT

---
+++ Register/src/register/Student.java
@@ -0,0 +1,64 @@
+package register;
+
+public class Student {
+	private Integer StudentID;
+	private String name;
+	private String RFID;
+	private boolean enrolled;
+	private boolean attend;
+	
+	public Student(int StudentID, String name, String RFID, boolean enrolled, boolean attend){
+		this.StudentID = StudentID;
+		this.name = name;
+		this.RFID = RFID;
+		this.enrolled = enrolled;
+		this.attend = attend;
+	}
+
+	public String getStudentID() {
+		return StudentID.toString();
+	}
+
+	public void setStudentID(int studentID) {
+		StudentID = studentID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRFID() {
+		return RFID;
+	}
+
+	public void setRFID(String rFID) {
+		RFID = rFID;
+	}
+
+	public boolean isEnrolled() {
+		return enrolled;
+	}
+
+	public void setEnrolled(boolean enrolled) {
+		this.enrolled = enrolled;
+	}
+
+	public boolean isAttend() {
+		return attend;
+	}
+
+	public void setAttend(boolean attend) {
+		this.attend = attend;
+	}
+	
+	public void changeAttend(){
+		if(attend == true)
+			attend = false;
+		else if(attend == false)
+			attend = true;
+	}
+}
---
+++ registerdb_Teacher.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE  IF NOT EXISTS `registerdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `registerdb`;
+-- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+--
+-- Host: mydb.c2abvobvruo6.ap-southeast-2.rds.amazonaws.com    Database: registerdb
+-- ------------------------------------------------------
+-- Server version	5.6.27-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Teacher`
+--
+
+DROP TABLE IF EXISTS `Teacher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Teacher` (
+  `teacherID` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `userName` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`teacherID`),
+  UNIQUE KEY `teacherID_UNIQUE` (`teacherID`),
+  UNIQUE KEY `userName_UNIQUE` (`userName`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Teacher`
+--
+
+LOCK TABLES `Teacher` WRITE;
+/*!40000 ALTER TABLE `Teacher` DISABLE KEYS */;
+INSERT INTO `Teacher` VALUES (11,'Greg','Greg','password'),(111,'Jane','Jane111','password');
+/*!40000 ALTER TABLE `Teacher` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-05-05 19:13:52
