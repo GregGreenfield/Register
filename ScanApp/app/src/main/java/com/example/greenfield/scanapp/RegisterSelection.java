230604885453300781eb49e906b6ba87718acdee
Greg Greenfield <g.r.greenfield@gmail.com>
2016-05-15 19:37:01 +1200

    Android Scanning app start

---
+++ ScanApp/app/src/main/java/com/example/greenfield/scanapp/RegisterSelection.java
@@ -0,0 +1,17 @@
+package com.example.greenfield.scanapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Greenfield on 14/05/2016.
+ */
+public class RegisterSelection extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registerselection);
+
+    }
+}
